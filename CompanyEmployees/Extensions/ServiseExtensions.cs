8c6ad30a477e58315960b7f67d966bee457e7bc2
1 <1@WS-1-1>
2022-10-10 16:15:48 +0300

    Добавьте файлы проекта.

---
+++ CompanyEmployees/Extensions/ServiseExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Contracts.Class1;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiseExtensions
+    {
+        public static void ConfigureCors(this Microsoft.Extensions.DependencyInjection.IServiceCollection services) =>
+ services.AddCors(options =>
+ {
+     options.AddPolicy("CorsPolicy", builder =>
+     builder.AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader());
+ });
+
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+ services.Configure<Microsoft.AspNetCore.Builder.IISOptions>(options =>
+ {
+
+ });
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+ services.AddScoped<ILoggerManager, ILoggerManager>();
+    }
+}
